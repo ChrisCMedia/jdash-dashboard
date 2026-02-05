f2847194260a66e2dada0446b8d2254b63fb2b9d
admin <admin@MacBook-Air.local>
2026-02-05 22:07:07 +0100

    Update Supabase configuration with real Anon Key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://quqplycygzbxcpjpujvf.supabase.co'
-const supabaseAnonKey = 'sb_publishable_1Fsz5dLVLY0U2-mZECNcBQ_kiPUc0oy'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1cXBseWN5Z3pieGNwanB1anZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMTYzMTUsImV4cCI6MjA4NTg5MjMxNX0.gzlYbNoMkayTkNkOjNTt8RN91MzsJLP-ukK2dMoVi7E'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
